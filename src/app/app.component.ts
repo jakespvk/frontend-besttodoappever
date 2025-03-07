60072a9e4d49f565db6bb4e76e9c5a95eed0547e
Jake Spievak <jspievak@oneinc.com>
2025-03-07 15:51:34 -0800

    updates to html, mainly feat: nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend-besttodoappever';
+  expanded = false;
+
+  expandNav() {
+    console.log("hi");
+    this.expanded ? this.expanded = false : this.expanded = true;
+  }
+
+  expandMobileNav() {
+    document.getElementById("mobileNav")?.showPopover();
+  }
+
 }
